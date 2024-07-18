00b0c749ebcfbb33d965004c605003f897352961
reddeppa <kamathamreddeppa2@gmail.com>
2024-07-18 22:46:28 +0530

    update the header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,41 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
-const Header = ()=>{
-    return(
-        <>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    
-    
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-   
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-       
-          < Link to="/home" class="nav-link" >Home</Link>
-        </li>
-        <li class="nav-item">
-        < Link to="/tour" class="nav-link" >Tours</Link>
-      
-        </li>
-        <li class="nav-item">
-        < Link to="/login" class="nav-link" >Login</Link>
-      
-        </li>
-        <li class="nav-item">
-        < Link to="/register" class="nav-link" >Register</Link>
-      
-        </li>
-        
-       
-      </ul>
-     
-    </div>
-  </div>
-</nav>
-        </>
-    )
-}
-export default Header
+const Header = () => {
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to="/home" class="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/tour" class="nav-link">
+                  Tours
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/login" class="nav-link">
+                  Login
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/register" class="nav-link">
+                  Register
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Header;
