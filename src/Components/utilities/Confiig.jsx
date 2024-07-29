410312e912525686f97697a286ac017e048dac00
reddeppa <kamathamreddeppa2@gmail.com>
2024-07-29 21:48:10 +0530

    changes  in public and baseurl

--- src/Components/utilities/Confiig.jsx
+++ src/Components/utilities/Confiig.jsx
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api/v1"
+export const BASE_URL = "https://travel-mern-j6ju.onrender.com/api/v1"
 export const token =localStorage.getItem("token")
 export const gettoken =()=>{
     const token=localStorage.getItem("token")
